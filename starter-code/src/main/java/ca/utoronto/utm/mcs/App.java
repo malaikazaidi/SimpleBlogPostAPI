eb47729988a16347be7b6becae029549bd01536c
Malaika Zaidi <malaika.zaidi@mail.utoronto.ca>
2020-03-01 11:40:49 -0500

    fixed minor bugs

--- starter-code/src/main/java/ca/utoronto/utm/mcs/App.java
+++ starter-code/src/main/java/ca/utoronto/utm/mcs/App.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
-
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.sun.net.httpserver.HttpServer;
 
 import javax.inject.Inject;
@@ -15,10 +12,11 @@ import javax.inject.Inject;
 public class App
 {
     static int port = 8080;
-
+    
     public static void main(String[] args) throws IOException
     {
     	Dagger service = DaggerDaggerComponent.create().buildMongoHttp();
+    	
     	//Create your server context here
     	service.getServer().createContext("/api/v1/post", new Post(service.getDb()));
     	service.getServer().start();
